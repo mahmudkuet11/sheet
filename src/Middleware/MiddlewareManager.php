319835a757b7d3d26bb62ead4320bcef2c795cbc
Md. Mahmud Ur Rahman <mahmudkuet11@gmail.com>
2019-12-06 17:06:45 +0600

    Add middleware support (#1)

---
+++ src/Middleware/MiddlewareManager.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by MD. Mahmud Ur Rahman <mahmud@mazegeek.com>.
+ */
+
+namespace Mahmud\Sheet\Middleware;
+
+
+class MiddlewareManager {
+    public function passThrough(array $middlewares, $row) {
+        foreach ($middlewares as $middleware){
+            if($middleware instanceof \Closure){
+                $row = $middleware($row);
+            }else{
+                $row = $middleware->handle($row);
+            }
+        }
+        
+        return $row;
+    }
+}
--- tests/SheetReaderTest.php
+++ tests/SheetReaderTest.php
@@ -27,4 +27,29 @@ class SheetReaderTest extends TestCase {
             'age'  => 26
         ], $data);
     }
+    
+    /**
+     * @test
+     */
+    public function middleware_can_be_applied_to_row() {
+        $data = [];
+        SheetReader::makeFromCsv(__DIR__ . "/dummy/files/test1.csv")
+            ->columns(['id', 'name', 'age'])
+            ->ignoreRow(0)
+            ->applyMiddleware(function ($row) {
+                $row['url'] = 'https://mahmud.live';
+                
+                return $row;
+            })
+            ->onEachRow(function ($row, $index) use (&$data) {
+                $data[] = $row;
+            })->read();
+        
+        $this->assertEquals([
+            'id'   => 1,
+            'name' => 'Mahmud',
+            'age'  => 26,
+            'url'  => 'https://mahmud.live'
+        ], $data[0]);
+    }
 }
